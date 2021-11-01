18e8d9511953dfc5c4029c3bb71fc14caf4bb701
AlexandrShepel <iam.alex.shepel@gmail.com>
2021-11-01 19:37:07 +0200

    Do task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+	gallery: document.querySelector(".gallery"),
+};
+
+const createItemMarkup = ({ preview, original, description }) =>
+	`<div class="gallery__item">
+		<a class="gallery__link" href="${preview}">
+			<img
+				class="gallery__image"
+				src="${preview}"
+				data-source="${original}"
+				alt="${description}"
+			/>
+		</a>
+	</div>`;
+
+const renderGalleryMarkup = galleryItems => {
+	refs.gallery.innerHTML = [...galleryItems]
+		.map(item => createItemMarkup(item))
+		.join("");
+};
+
+renderGalleryMarkup(galleryItems);
+
+new SimpleLightbox(".gallery a");
+
+refs.gallery.addEventListener("click", onClickGallery);
