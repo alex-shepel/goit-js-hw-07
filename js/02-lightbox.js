0c277b82b22c3de3d85926de3876cd4309bc708c
AlexandrShepel <iam.alex.shepel@gmail.com>
2021-11-01 19:44:06 +0200

    Fix img src bug task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,11 +7,10 @@ const refs = {
 
 const createItemMarkup = ({ preview, original, description }) =>
 	`<div class="gallery__item">
-		<a class="gallery__link" href="${preview}">
+		<a class="gallery__link" href="${original}">
 			<img
 				class="gallery__image"
 				src="${preview}"
-				data-source="${original}"
 				alt="${description}"
 			/>
 		</a>
